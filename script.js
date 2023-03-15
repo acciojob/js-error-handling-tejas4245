8f984e0c357691df60747a037a967ab462fe09ed
tejas4245 <120262626+tejas4245@users.noreply.github.com>
2023-03-15 09:16:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your code here
+class OutOfRangeError extends Error{
+	constructor(){
+		super("Expression should only consist of integers and +-/* characters");
+		this.name="OutOfRangeError";
+	}
+}
+
+class InvalidExprError extends Error{
+	constructor(){
+		super("Expression should not have an invalid combination of expression");
+		this.name="InvalidExprError";
+	}
+}
+
+function evalString(expression){
+	try{
+		if(expression.match(/[^0-9+\-*/]/)){
+			throw new OutOfRangeError();
+		}
+		if(expression.match(/\+{2,}|\-{2,}|\*{2,}|\/{2,}/)){
+			throw new InvalidExprError();
+		}
+		if(expression.match(/^[\/*+]/)){
+			throw new SyntaxError("Expression should not start with invalid operator");
+		}
+		if(expression.match(/[+\-*/]$/)){
+			throw new SyntaxError("Expression should not end with invalid operator");
+		}
+		let result=eval(expression);
+		return result;
+	}catch(error){
+		console.error(error);
+		return null;
+	}
+}
